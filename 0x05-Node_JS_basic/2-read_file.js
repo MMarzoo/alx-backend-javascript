89a4608cd7ba1823d8fddaefaf54ddff3b887711
MMarzoo <mmarzouk12378@gmail.com>
2024-09-17 18:53:01 +0300

    Task #2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,23 +1,25 @@
 const fs = require('fs');
 
 function filterByGroup(group, dataset) {
-    return dataset.filter((entry) => entry.split(',').at(-1).toString() === group);
-}
-function countStudents(path) {
-    try {
-        const data = fs.readFileSync(path, 'utf-8');
-        const Sdata = data.toString().trim().split('\n');
-        const fulldata = Sdata.splice(1,data.length);
-        console.log(`Number of students: ${fulldata.length}`);
-        const groups = new Set(fulldata.map((item) => item.split(',').at(-1)));
-        for (const group of groups) {
-            const filteredgroup = filterByGroup(group, fulldata);
-            const fname = filteredgroup.map((entry) => entry.split(',').at(0));
-            console.log(`Number of students in ${group}: ${filteredgroup.length}. List: ${fname.join(', ')}`);
-        }
-    } catch (error) {
-        throw new Error('Cannot load the database');
-    }
+  return dataset.filter((entry) => entry.split(',').at(-1).toString() === group);
 }
 
-module.exports = countStudents
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    // get rid of the first line in the csv
+    const sData = data.toString().trim().split('\n');
+    const fullData = sData.splice(1, data.length);
+    console.log(`Number of students: ${fullData.length}`);
+    const groups = new Set(fullData.map((item) => item.split(',').at(-1)));
+    for (const group of groups) {
+      const filteredGroup = filterByGroup(group, fullData);
+      const fNames = filteredGroup.map((entry) => entry.split(',').at(0));
+      console.log(`Number of students in ${group}: ${filteredGroup.length}. List: ${fNames.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
