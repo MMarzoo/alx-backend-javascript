7321e3569eb83a8613a8e1b53e6e15d2f966562d
MMarzoo <mmarzouk12378@gmail.com>
2024-09-17 19:22:28 +0300

    Task #4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,11 +5,11 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello Holberton School!');
+  res.end('Hello Holberton School!')
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}`)
 });
 
 module.exports = app;
